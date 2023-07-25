bed3842bb90b423a791ca7b72001390d297e53f6
diazarm <marceloardiaz@gmail.com>
2023-07-25 10:15:48 -0400

    update

--- server/src/controllers/user/putUserController.js
+++
@@ -1,8 +0,0 @@
-const {User}=require('../../db');
-
-const putUserController = async (name,gender,phone,date,email,admin,status) => {  
-    const newUser= await User.update({name,gender,phone,date,admin,status},{where:{email:email}});
-    if(newUser) return newUser;
-    else throw new Error('no se encontro un email valido');   
-    }    
- module.exports = putUserController; 
--- server/src/routes/lessonRoute.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const getLessonHandler = require('../handlers/lesson/getLessonHandler')
-
-const route = Router();
-
-route.get('/all',getLessonHandler); //Trae todas las materias
-
-module.exports = route;
